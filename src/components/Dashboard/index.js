5c6112aab70a3d184c8f9e575ab1c8673082edf0
02loveslollipop <mmonsalve2005@gmail.com>
2024-04-11 10:12:58 -0500

    Fix import statement in Dashboard component

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-//import Swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 import './Dashboard.css';
 import Header from './Header';
 import Table from './Table';

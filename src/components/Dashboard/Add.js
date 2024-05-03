5b76a99138b3f9c6d7f7bce2f801ff673551f93d
Arroyave03 <arroyave0322@hotmail.com>
2024-05-02 20:11:37 -0500

    Fixed add UI

--- src/components/Dashboard/Add.js
+++ src/components/Dashboard/Add.js
@@ -5,7 +5,7 @@ import Swal from 'sweetalert2';
 {"Id": "1", "name": "test", "plate": "test", "invoice": "test", "inicial_time": "test"}
 */
 
-async function addParking(name,plate,invoice,inicial_time,final_time) {
+async function addParking(name, plate, invoice, inicial_time, final_time) {
   const url = "https://02loveslollipop.pythonanywhere.com/insert"; // TODO: no quemar la URL
   const data = {
     name: name,
@@ -19,8 +19,8 @@ async function addParking(name,plate,invoice,inicial_time,final_time) {
   const response = await fetch(url, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json', 
-      'hash' : localStorage.getItem('secretAuth')
+      'Content-Type': 'application/json',
+      'hash': localStorage.getItem('secretAuth')
     },
     body: JSON.stringify(data)
   });
@@ -77,60 +77,64 @@ const Add = ({ employees, setEmployees, setIsAdding }) => {
   };
 
   return (
-    <div className="small-container">
-      <form onSubmit={handleAdd}>
-        <h1>Añadir vehículo</h1>
-        <label htmlFor="firstName">Nombre</label>
-        <input
-          id="firstName"
-          type="text"
-          name="firstName"
-          value={firstName}
-          onChange={e => setFirstName(e.target.value)}
-        />
-        <label htmlFor="lastName">Placa</label>
-        <input
-          id="lastName"
-          type="text"
-          name="lastName"
-          value={lastName}
-          onChange={e => setLastName(e.target.value)}
-        />
-        <label htmlFor="email">Valor parqueadero ($)</label>
-        <input
-          id="email"
-          type="number"
-          name="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-        <label htmlFor="salary">Fecha inicio</label>
-        <input
-          id="salary"
-          type="date"
-          name="salary"
-          value={salary}
-          onChange={e => setSalary(e.target.value)}
-        />
-        <label htmlFor="date">Fecha fin</label>
-        <input
-          id="date"
-          type="date"
-          name="date"
-          value={date}
-          onChange={e => setDate(e.target.value)}
-        />
-        <div style={{ marginTop: '30px' }}>
-          <input type="submit" value="Agregar" />
-          <input
-            style={{ marginLeft: '12px' }}
-            className="muted-button"
-            type="button"
-            value="Cancelar"
-            onClick={() => setIsAdding(false)}
-          />
+    <div className='edit-screen'>
+      <div classname="edit-container">
+        <div className="edit-box">
+          <form onSubmit={handleAdd}>
+            <h1>Añadir vehículo</h1>
+            <label htmlFor="firstName">Nombre</label>
+            <input
+              id="firstName"
+              type="text"
+              name="firstName"
+              value={firstName}
+              onChange={e => setFirstName(e.target.value)}
+            />
+            <label htmlFor="lastName">Placa</label>
+            <input
+              id="lastName"
+              type="text"
+              name="lastName"
+              value={lastName}
+              onChange={e => setLastName(e.target.value)}
+            />
+            <label htmlFor="email">Valor parqueadero ($)</label>
+            <input
+              id="email"
+              type="number"
+              name="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <label htmlFor="salary">Fecha inicio</label>
+            <input
+              id="salary"
+              type="date"
+              name="salary"
+              value={salary}
+              onChange={e => setSalary(e.target.value)}
+            />
+            <label htmlFor="date">Fecha fin</label>
+            <input
+              id="date"
+              type="date"
+              name="date"
+              value={date}
+              onChange={e => setDate(e.target.value)}
+            />
+            <div style={{ marginTop: '30px' }}>
+              <input type="submit" value="Agregar" />
+              <input
+                style={{ marginLeft: '12px' }}
+                className="muted-button"
+                type="submit"
+                value="Cancelar"
+                onClick={() => setIsAdding(false)}
+              />
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
